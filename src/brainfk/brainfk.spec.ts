382dfe4ca70a8ea376fd4c8123db66d4ae8c2732
Axel <axel.bie@metronlab.com>
2020-10-21 17:03:43 +0200

    feat(): Adds first attempt of Brain fk interpretor

---
+++ src/brainfk/brainfk.spec.ts
@@ -0,0 +1,81 @@
+import { assert } from 'chai';
+import { brainLuck, getBracketPairs } from './brainfk';
+
+describe('getBrackets', () => {
+  it('returns an empty array', () => {
+    assert.deepEqual(getBracketPairs(''), []);
+  });
+
+  it('returns one bracket pair', () => {
+    assert.deepEqual(getBracketPairs('[]'), [{ open: 0, close: 1 }]);
+    assert.deepEqual(getBracketPairs('01[]'), [{ open: 2, close: 3 }]);
+    assert.deepEqual(getBracketPairs('01[34]'), [{ open: 2, close: 5 }]);
+  });
+
+  it('returns 2 bracket pairs', () => {
+    assert.deepEqual(getBracketPairs('[][]'), [
+      { open: 0, close: 1 },
+      { open: 2, close: 3 },
+    ]);
+    assert.deepEqual(getBracketPairs('01[3[56]]9'), [
+      { open: 4, close: 7 },
+      { open: 2, close: 8 },
+    ]);
+  });
+});
+
+describe('tdd style', () => {
+  it('returns an empty string when no program provided', () => {
+    assert.equal(brainLuck('', 'codewars'), '');
+  });
+
+  it('can print the pointed value', () => {
+    assert.equal(brainLuck('.', ''), String.fromCharCode(0));
+    assert.equal(brainLuck('...', ''), String.fromCharCode(0) + String.fromCharCode(0) + String.fromCharCode(0));
+  });
+
+  it('can read from input', () => {
+    assert.equal(brainLuck(',.', String.fromCharCode(45)), String.fromCharCode(45));
+    assert.equal(brainLuck(',.,.,.', 'ABC'), 'ABC');
+  });
+
+  it('can increment a value', () => {
+    assert.equal(brainLuck(',+.', 'A'), 'B');
+    // Byte increment condition 255 + 1 = 0
+    assert.equal(brainLuck(',+.', String.fromCharCode(255)), String.fromCharCode(0));
+  });
+
+  it('can decrement a value', () => {
+    assert.equal(brainLuck(',-.', 'B'), 'A');
+    // Byte increment condition 0 - 1 = 255
+    assert.equal(brainLuck(',-.', String.fromCharCode(0)), String.fromCharCode(255));
+  });
+
+  it('should move pointer to the right', () => {
+    assert.equal(brainLuck('+++>,.', 'A'), 'A');
+    assert.equal(brainLuck('+++>,>>>>.', 'A'), String.fromCharCode(0));
+  });
+
+  it('should move pointer to the left', () => {
+    assert.equal(brainLuck(',>++++<.', 'A'), 'A');
+  });
+
+  it('go through a while', () => {
+    assert.equal(brainLuck('[].', String.fromCharCode(0)), String.fromCharCode(0));
+    assert.equal(brainLuck(',[-].', String.fromCharCode(0)), String.fromCharCode(0));
+  });
+});
+
+describe('brainLuck', () => {
+  it('Echo until byte(255) encountred', () => {
+    assert.equal(brainLuck(',+[-.,+]', 'Codewars' + String.fromCharCode(255)), 'Codewars');
+  });
+
+  it('Echo until byte(0) encountred', () => {
+    assert.equal(brainLuck(',[.[-],]', 'Codewars' + String.fromCharCode(0)), 'Codewars');
+  });
+
+  it('Two numbers multiplier', () => {
+    assert.equal(brainLuck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', String.fromCharCode(8, 9)), String.fromCharCode(72));
+  });
+});
---
+++ src/brainfk/brainfk.ts
@@ -0,0 +1,166 @@
+class InputReader {
+  readonly input: number[];
+  private index: number;
+  constructor(input: string) {
+    this.input = input.split('').map((c) => c.charCodeAt(0));
+    this.index = 0;
+  }
+
+  readInput() {
+    const readInput = this.input[this.index];
+    this.index++;
+
+    return readInput;
+  }
+}
+
+class Byte {
+  public value: number;
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  increment() {
+    this.value = this.value === 255 ? 0 : this.value + 1;
+  }
+
+  decrement() {
+    this.value = this.value === 0 ? 255 : this.value - 1;
+  }
+}
+
+class Memory {
+  private content: Byte[];
+  constructor() {
+    this.initContent();
+  }
+
+  private initContent() {
+    this.content = [];
+    for (let i = 0; i < 4000; i++) {
+      this.content.push(new Byte(0));
+    }
+  }
+
+  increment(pointer: number) {
+    this.content[pointer].increment();
+  }
+
+  decrement(pointer: number) {
+    this.content[pointer].decrement();
+  }
+
+  read(pointer: number): number {
+    return this.content[pointer].value;
+  }
+
+  write(pointer: number, value: number) {
+    this.content[pointer].value = value;
+  }
+}
+
+class Output {
+  public value: string;
+
+  constructor() {
+    this.value = '';
+  }
+
+  out(value: number) {
+    this.value += String.fromCharCode(value);
+  }
+}
+
+type BracketPair = {
+  open: number;
+  close: number;
+};
+
+export function getBracketPairs(inputString: string): BracketPair[] {
+  console.log(inputString);
+  if (inputString.length === 0) {
+    return [];
+  }
+  const inputArray = inputString.split('');
+  const incompletePairs: BracketPair[] = [];
+  const completePairs: BracketPair[] = [];
+
+  inputArray.forEach((char, index) => {
+    if (char === '[') {
+      incompletePairs.push({ open: index, close: 0 });
+    }
+    if (char === ']') {
+      const newPair = incompletePairs.pop();
+      newPair.close = index;
+      completePairs.push(newPair);
+    }
+  });
+  return completePairs;
+}
+
+function getOpeningBracketIndex(bracketPairs: BracketPair[], closingIndex: number): number {
+  const pair = bracketPairs.find((pair: BracketPair) => pair.close === closingIndex);
+  return pair.open;
+}
+
+function getClosingBracketIndex(bracketPairs: BracketPair[], openingIndex: number): number {
+  const pair = bracketPairs.find((pair: BracketPair) => pair.open === openingIndex);
+  return pair.close;
+}
+
+export function brainLuck(code: string, inputString: string): string {
+  const memory = new Memory();
+  const inputReader = new InputReader(inputString);
+  const output = new Output();
+
+  let pointer = 0;
+
+  const codeArray = code.split('');
+  let symbol;
+
+  const brackets = getBracketPairs(code);
+
+  for (let i = 0; i < codeArray.length; i++) {
+    symbol = codeArray[i];
+
+    if (symbol === '.') {
+      const value = memory.read(pointer);
+      output.out(value);
+    }
+
+    if (symbol === ',') {
+      const value = inputReader.readInput();
+      memory.write(pointer, value);
+    }
+
+    if (symbol === '+') {
+      memory.increment(pointer);
+    }
+
+    if (symbol === '-') {
+      memory.decrement(pointer);
+    }
+
+    if (symbol === '>') {
+      pointer++;
+    }
+
+    if (symbol === '<') {
+      pointer--;
+    }
+
+    if (symbol === '[') {
+      if (memory.read(pointer) === 0) {
+        i = getClosingBracketIndex(brackets, i);
+      }
+    }
+
+    if (symbol === ']') {
+      if (memory.read(pointer) !== 0) {
+        i = getOpeningBracketIndex(brackets, i);
+      }
+    }
+  }
+
+  return output.value;
+}
